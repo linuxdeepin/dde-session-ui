0cba8606c8ad69ac83ebf9a4b0bf1e76cc3ce980
石博文 <sbw@sbw.so>
2015-11-06 15:54:53 +0800

    internationalization support
    Change-Id: I3ff3b67e84177efed55fd651a15fcdbbb77ed75e

--- translations/dde-session-ui_zh_CN.ts
+++ translations/dde-session-ui_zh_CN.ts
@@ -2,19 +2,47 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../dde-zone/mainwindow.cpp" line="22"/>
+        <source>Control Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-zone/mainwindow.cpp" line="22"/>
+        <source>All Windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-zone/mainwindow.cpp" line="22"/>
+        <source>Launcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-zone/mainwindow.cpp" line="22"/>
+        <source>Show Desktop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-zone/mainwindow.cpp" line="22"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>ShutDownFrame</name>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="47"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="53"/>
         <source>Shut down</source>
         <translation>关机</translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="54"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="56"/>
         <source>Restart</source>
         <translation>重启</translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="58"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="59"/>
         <source>Suspend</source>
         <translation>待机</translation>
     </message>
@@ -24,9 +52,14 @@
         <translation>锁定</translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="66"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="65"/>
         <source>Log out</source>
         <translation>注销</translation>
+    </message>
+    <message>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="68"/>
+        <source>Switch user</source>
+        <translation>切换用户</translation>
     </message>
 </context>
 <context>
