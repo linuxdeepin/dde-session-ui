2c4a63b3469ad45ccb5c5a6d65be5709ee2327fc
石博文 <sbw@sbw.so>
2015-12-03 17:01:27 +0800

    set all monitors extend
    https://tower.im/projects/715dbdd34f694ea08f6de7888b7ffd7d/todos/83f3bb366bf0418a80546c712850075f/
    https://tower.im/projects/715dbdd34f694ea08f6de7888b7ffd7d/todos/5991541a07d04ff7aea69dbc3de1da3c/
    Change-Id: I1b2bd8ee68ceeb1f562089322c5aac42d0ca15e7

--- lightdm-deepin-greeter/main.cpp
+++ lightdm-deepin-greeter/main.cpp
@@ -2,14 +2,69 @@
 #include <QtCore/QTranslator>
 #include "app/loginframe.h"
 #include <QLabel>
+#include <QProcess>
 
 #include "logmanager.h"
 
+void setAllMonitorsExtend()
+{
+    QProcess *checkMons = new QProcess;
+    checkMons->start("xrandr");
+    checkMons->waitForFinished(1000);
+
+    QString primaryMonitor;
+    QStringList otherMonitors;
+
+    const QString result = checkMons->readAllStandardOutput();
+    const QStringList &infoList = result.split('\n');
+    for (const QString &info : infoList)
+    {
+        qDebug() << "info: " << info;
+
+        const QStringList details = info.split(' ');
+
+        if (details.count() < 3 || details.at(1) != "connected")
+            continue;
+
+        qDebug() << "found monitor: " << details.first();
+
+        if (details.at(2) == "primary")
+            primaryMonitor = details.first();
+        else
+            otherMonitors.append(details.first());
+    }
+
+    // set other monitors
+    QString lastMonitor = primaryMonitor;
+    if (lastMonitor.isEmpty())
+        lastMonitor = otherMonitors.first();
+
+    for (const QString &m : otherMonitors)
+    {
+        if (m == lastMonitor)
+            continue;
+
+        QProcess *setting = new QProcess;
+        QString cmd = QString("xrandr --output %1 --right-of %2 --auto").arg(m).arg(lastMonitor);
+
+        qDebug() << "exec: " << cmd;
+
+        setting->start(cmd);
+        bool result = setting->waitForFinished(1000);
+        qDebug() << "finished: " << result;
+        setting->deleteLater();
+    }
+
+    checkMons->deleteLater();
+}
+
 int main(int argc, char* argv[])
 {
+    LogManager::instance()->debug_log_console_on();
+    setAllMonitorsExtend();
+
     QApplication a(argc, argv);
 
-//    LogManager::instance()->debug_log_console_on();
     /* the logmanager is used to output debug log */
     QTranslator translator;
     translator.load("/usr/share/dde-session-ui/translations/dde-session-ui_" + QLocale::system().name());
