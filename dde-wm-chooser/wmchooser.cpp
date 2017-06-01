ba4c005ee2f765ab45f5cc58b6ffbcef03dff5a2
kirigaya <kirigaya@mkacg.com>
2017-06-01 09:14:34 +0800

    dde-wm-chooser: Fix wrong words
    Change-Id: Ifa3ce2273e28c33e482553dbbd639ac4f7937718

--- dde-wm-chooser/wmchooser.cpp
+++ dde-wm-chooser/wmchooser.cpp
@@ -42,7 +42,7 @@ WMChooser::WMChooser(QWidget *parent)
     QLabel*tip = new QLabel(tr("System has detected that you are using a virtual machine, which will affect the system performance and operation experience, for a smooth experience, it is recommended to select Common Mode"));
     tip->setWordWrap(true);
 
-    QLabel *bottomTip = new QLabel(tr("Effect Mode: You can soomthly and gorgeously experience. Common Mode: You can extremely rapidly experience"));
+    QLabel *bottomTip = new QLabel(tr("Effect Mode: You can smoothly and gorgeously experience. Common Mode: You can extremely rapidly experience"));
     tip->setStyleSheet("line-height: 1.43;"
                        "color: #ffffff;"
                        "font-size: 14px;");
