5908b90147b99269c5741cf26ede7c83d338a5d0
kirigaya <kirigaya@mkacg.com>
2017-06-08 09:14:22 +0800

    dde-wm-chooser: fix not load translation
    Change-Id: Id0fcd36418b5a0e4d5b2e1b8e93071c35f7e66cf

--- dde-wm-chooser/main.cpp
+++ dde-wm-chooser/main.cpp
@@ -3,6 +3,7 @@
 #include <QCommandLineOption>
 #include <QCommandLineParser>
 #include <QDebug>
+#include <QTranslator>
 
 DWIDGET_USE_NAMESPACE
 
@@ -11,7 +12,10 @@ int main(int argc, char *argv[])
     DApplication::loadDXcbPlugin();
     DApplication a(argc, argv);
     a.setApplicationName("deepin-wm-chooser");
-    a.loadTranslator();
+
+    QTranslator translator;
+    translator.load("/usr/share/dde-session-ui/translations/dde-session-ui_" + QLocale::system().name());
+    a.installTranslator(&translator);
 
     QCommandLineOption config(QStringList() << "c" << "config", "");
     config.setValueName("ConfigPath");
@@ -22,7 +26,6 @@ int main(int argc, char *argv[])
     parser.process(a);
 
     WMFrame w;
-    w.show();
     if (parser.isSet(config)) {
         w.setConfigPath(parser.value(config));
         w.show();
