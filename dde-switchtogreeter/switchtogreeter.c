9ad9370f2a60f4cafdb087e66b6caffc33e6b316
石博文 <sbw@sbw.so>
2017-09-18 16:15:34 +0800

    update license
    Change-Id: I48faff7c9aa1b5a5929c785388de6f82b294e276

--- dde-switchtogreeter/switchtogreeter.c
+++ dde-switchtogreeter/switchtogreeter.c
@@ -1,15 +1,22 @@
-/**
- * Copyright (c) 2011 ~ 2013 Deepin, Inc.
+/*
+ * Copyright (C) 2011 ~ 2013 Deepin Technology Co., Ltd.
  *               2011 ~ 2013 Long Wei
  *
- * Author:      Long Wei <yilang2007lw@gmail.com>
- *              bluth <yuanchenglu001@gmail.com>
- * Maintainer:  Long Wei <yilang2007lw@gamil.com>
+ * Author:     sbw <sbw@sbw.so>
+ *             kirigaya <kirigaya@mkacg.com>
+ *             bluth <yuanchenglu001@gmail.com>
+ *             Hualet <mr.asianwang@gmail.com>
+ *             Long Wei <yilang2007lw@gmail.com>
  *
- * This program is free software; you can redistribute it and/or modify
+ * Maintainer: sbw <sbw@sbw.so>
+ *             kirigaya <kirigaya@mkacg.com>
+ *             Hualet <mr.asianwang@gmail.com>
+ *             Long Wei <yilang2007lw@gamil.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +24,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
- **/
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #define _POSIX_SOURCE
 #include <sys/types.h>
