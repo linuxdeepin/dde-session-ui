b7191f707fc0dd5f07b10343edf2429ecbe3d09d
kirigaya <kirigaya@mkacg.com>
2017-10-17 10:12:01 +0800

    update ts file
    Change-Id: Ie6587a3d02a26bc991c807bda92ba092886e7b73

--- translations/dde-session-ui.ts
+++ translations/dde-session-ui.ts
@@ -4,52 +4,52 @@
 <context>
     <name>ContentWidget</name>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="215"/>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="261"/>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="349"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="244"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="290"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="378"/>
         <source>Shut down</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="217"/>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="266"/>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="352"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="246"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="295"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="381"/>
         <source>Restart</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="262"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="291"/>
         <source>Are you sure to shut down?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="267"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="296"/>
         <source>Are you sure to restart?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="271"/>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="361"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="300"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="390"/>
         <source>Log out</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="272"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="301"/>
         <source>Are you sure to log out?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="355"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="384"/>
         <source>Suspend</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="358"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="387"/>
         <source>Lock</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/contentwidget.cpp" line="365"/>
+        <location filename="../dde-shutdown/view/contentwidget.cpp" line="394"/>
         <source>Switch user</source>
         <translation type="unfinished"></translation>
     </message>
@@ -57,12 +57,12 @@
 <context>
     <name>DisplayModeProvider</name>
     <message>
-        <location filename="../dde-osd/displaymodeprovider.cpp" line="133"/>
+        <location filename="../dde-osd/displaymodeprovider.cpp" line="158"/>
         <source>Duplicate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-osd/displaymodeprovider.cpp" line="135"/>
+        <location filename="../dde-osd/displaymodeprovider.cpp" line="160"/>
         <source>Extend</source>
         <translation type="unfinished"></translation>
     </message>
@@ -70,7 +70,7 @@
 <context>
     <name>InhibitWarnView</name>
     <message>
-        <location filename="../dde-shutdown/view/inhibitwarnview.cpp" line="25"/>
+        <location filename="../dde-shutdown/view/inhibitwarnview.cpp" line="50"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
@@ -78,7 +78,7 @@
 <context>
     <name>KBLayoutIndicator</name>
     <message>
-        <location filename="../dde-osd/kblayoutindicator.cpp" line="127"/>
+        <location filename="../dde-osd/kblayoutindicator.cpp" line="152"/>
         <source>Add keyboard layout</source>
         <translation type="unfinished"></translation>
     </message>
@@ -86,22 +86,22 @@
 <context>
     <name>LockManager</name>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="129"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="158"/>
         <source>Login</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="224"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="405"/>
         <source>Wrong Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="494"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="559"/>
         <source>Enter your password to restart</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="497"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="564"/>
         <source>Enter your password to shutdown</source>
         <translation type="unfinished"></translation>
     </message>
@@ -109,7 +109,7 @@
 <context>
     <name>LoginManager</name>
     <message>
-        <location filename="../lightdm-deepin-greeter/app/loginmanager.cpp" line="475"/>
+        <location filename="../lightdm-deepin-greeter/app/loginmanager.cpp" line="493"/>
         <source>Wrong Password</source>
         <translation type="unfinished"></translation>
     </message>
@@ -117,12 +117,12 @@
 <context>
     <name>MultiUsersWarningView</name>
     <message>
-        <location filename="../dde-shutdown/view/multiuserswarningview.cpp" line="98"/>
+        <location filename="../dde-shutdown/view/multiuserswarningview.cpp" line="124"/>
         <source>The above users still keep logged in and the data will be lost due to shutdown, are you sure to shut down? </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-shutdown/view/multiuserswarningview.cpp" line="104"/>
+        <location filename="../dde-shutdown/view/multiuserswarningview.cpp" line="130"/>
         <source>The above users still keep logged in and the data will be lost due to reboot, are you sure to reboot? </source>
         <translation type="unfinished"></translation>
     </message>
@@ -130,17 +130,17 @@
 <context>
     <name>ShutdownWidget</name>
     <message>
-        <location filename="../widgets/shutdownwidget.cpp" line="29"/>
+        <location filename="../widgets/shutdownwidget.cpp" line="45"/>
         <source>Shut down</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../widgets/shutdownwidget.cpp" line="33"/>
+        <location filename="../widgets/shutdownwidget.cpp" line="49"/>
         <source>Restart</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../widgets/shutdownwidget.cpp" line="37"/>
+        <location filename="../widgets/shutdownwidget.cpp" line="53"/>
         <source>Suspend</source>
         <translation type="unfinished"></translation>
     </message>
@@ -148,23 +148,23 @@
 <context>
     <name>SuspendDialog</name>
     <message>
-        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="18"/>
+        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="34"/>
         <source>External monitor detected, suspend?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="18"/>
-        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="36"/>
+        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="34"/>
+        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="55"/>
         <source>%1s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="26"/>
+        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="45"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="26"/>
+        <location filename="../dde-suspend-dialog/suspenddialog.cpp" line="45"/>
         <source>Suspend</source>
         <translation type="unfinished"></translation>
     </message>
@@ -172,7 +172,7 @@
 <context>
     <name>SystemMonitor</name>
     <message>
-        <location filename="../dde-shutdown/view/systemmonitor.cpp" line="22"/>
+        <location filename="../dde-shutdown/view/systemmonitor.cpp" line="47"/>
         <source>Start system monitor</source>
         <translation type="unfinished"></translation>
     </message>
@@ -180,40 +180,58 @@
 <context>
     <name>TimeWidget</name>
     <message>
-        <location filename="../dde-lock/timewidget.cpp" line="59"/>
+        <location filename="../dde-lock/timewidget.cpp" line="76"/>
         <source>hh:mm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/timewidget.cpp" line="60"/>
+        <location filename="../dde-lock/timewidget.cpp" line="77"/>
         <source>yyyy-MM-dd dddd</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdateContent</name>
+    <message>
+        <location filename="../dde-welcome/updatecontent.cpp" line="41"/>
+        <source>Welcome, system updated successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-welcome/updatecontent.cpp" line="42"/>
+        <source>Current Edition:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-welcome/updatecontent.cpp" line="43"/>
+        <source>Enter</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>WMChooser</name>
     <message>
-        <location filename="../dde-wm-chooser/wmchooser.cpp" line="31"/>
+        <location filename="../dde-wm-chooser/wmchooser.cpp" line="56"/>
         <source>Effect Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-wm-chooser/wmchooser.cpp" line="34"/>
+        <location filename="../dde-wm-chooser/wmchooser.cpp" line="59"/>
         <source>Common Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-wm-chooser/wmchooser.cpp" line="38"/>
+        <location filename="../dde-wm-chooser/wmchooser.cpp" line="63"/>
         <source>Friendly Reminder</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-wm-chooser/wmchooser.cpp" line="42"/>
+        <location filename="../dde-wm-chooser/wmchooser.cpp" line="67"/>
         <source>System has detected that you are using a virtual machine, which will affect the system performance and operation experience, for a smooth experience, it is recommended to select Common Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-wm-chooser/wmchooser.cpp" line="45"/>
+        <location filename="../dde-wm-chooser/wmchooser.cpp" line="70"/>
         <source>Effect Mode: You can smoothly and gorgeously experience. Common Mode: You can extremely rapidly experience</source>
         <translation type="unfinished"></translation>
     </message>
@@ -221,17 +239,17 @@
 <context>
     <name>WMStateProvider</name>
     <message>
-        <location filename="../dde-osd/wmstateprovider.cpp" line="18"/>
+        <location filename="../dde-osd/wmstateprovider.cpp" line="43"/>
         <source>Window effect enabled</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-osd/wmstateprovider.cpp" line="20"/>
+        <location filename="../dde-osd/wmstateprovider.cpp" line="45"/>
         <source>Window effect disabled</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-osd/wmstateprovider.cpp" line="22"/>
+        <location filename="../dde-osd/wmstateprovider.cpp" line="47"/>
         <source>Failed to enable window effects</source>
         <translation type="unfinished"></translation>
     </message>
@@ -239,17 +257,17 @@
 <context>
     <name>WarningDialog</name>
     <message>
-        <location filename="../dde-warning-dialog/warningdialog.cpp" line="9"/>
+        <location filename="../dde-warning-dialog/warningdialog.cpp" line="38"/>
         <source>Kindly Reminder</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-warning-dialog/warningdialog.cpp" line="10"/>
+        <location filename="../dde-warning-dialog/warningdialog.cpp" line="39"/>
         <source>This application can not run without window effect</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-warning-dialog/warningdialog.cpp" line="14"/>
+        <location filename="../dde-warning-dialog/warningdialog.cpp" line="43"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
@@ -257,7 +275,7 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="../dde-lowpower/window.cpp" line="23"/>
+        <location filename="../dde-lowpower/window.cpp" line="39"/>
         <source>Low battery, please plug in</source>
         <translation type="unfinished"></translation>
     </message>
