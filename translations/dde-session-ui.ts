a3b11edbe39c63f8915b13b547dc694ca7b78319
kirigaya <kirigaya@mkacg.com>
2017-10-25 15:32:15 +0800

    update ts
    Change-Id: I7f70c4e11a6f21f61c9764db322597b9487e172d

--- translations/dde-session-ui.ts
+++ translations/dde-session-ui.ts
@@ -86,22 +86,27 @@
 <context>
     <name>LockManager</name>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="158"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="154"/>
         <source>Login</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="413"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="405"/>
+        <source>Please enter your password manually if fingerprint password timed out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dde-lock/lockmanager.cpp" line="420"/>
         <source>Wrong Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="567"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="550"/>
         <source>Enter your password to reboot</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dde-lock/lockmanager.cpp" line="572"/>
+        <location filename="../dde-lock/lockmanager.cpp" line="555"/>
         <source>Enter your password to shutdown</source>
         <translation type="unfinished"></translation>
     </message>
@@ -109,7 +114,17 @@
 <context>
     <name>LoginManager</name>
     <message>
-        <location filename="../lightdm-deepin-greeter/app/loginmanager.cpp" line="505"/>
+        <location filename="../lightdm-deepin-greeter/app/loginmanager.cpp" line="297"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lightdm-deepin-greeter/app/loginmanager.cpp" line="473"/>
+        <source>Please enter your password manually if fingerprint password timed out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../lightdm-deepin-greeter/app/loginmanager.cpp" line="527"/>
         <source>Wrong Password</source>
         <translation type="unfinished"></translation>
     </message>
