64c1c65a3b20367ecfbe16ad17cc925b9bc3f073
石博文 <sbw@sbw.so>
2018-01-18 15:46:19 +0800

    remove tabs prefix
    Change-Id: Ia62537ba1f73f0c0e5893a39cf07b293ba137473

--- dmemory-warning-dialog/src/buttondelegate.h
+++ dmemory-warning-dialog/src/buttondelegate.h
@@ -12,7 +12,6 @@ public:
 
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
     bool editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option, const QModelIndex &index);
-    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
 };
 
 #endif // BUTTONDELEGATE_H
