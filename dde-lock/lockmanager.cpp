651522b7e0bebfe6158bc7fc3714abeb9648abc9
haruyukilxz <kirigaya@mkacg.com>
2018-04-13 09:57:35 +0800

    refactor(lock): restore background change signal
    Change-Id: Ie0dc7d93c8bc26056a7f23ec553bd7234c2a86e1

--- dde-lock/lockmanager.cpp
+++ dde-lock/lockmanager.cpp
@@ -68,6 +68,10 @@ LockManager::LockManager(QWidget *parent)
     });
 
     m_currentUser = m_userWidget->currentUser();
+
+#ifdef LOCK_NO_QUIT
+    connect(m_currentUser, &User::greeterBackgroundPathChanged, this, &LockManager::backgroundChanged);
+#endif
 }
 
 void LockManager::initConnect()
@@ -444,9 +448,7 @@ void LockManager::onCurrentUserChanged(User *user)
 
     updatePasswordEditVisible(m_currentUser);
 
-    const QString &wallpaper = m_blurImageInter->Get(user->greeterBackgroundPath());
-
-    emit requestSetBackground(wallpaper.isEmpty() ? user->greeterBackgroundPath() : wallpaper);
+    backgroundChanged(m_currentUser->greeterBackgroundPath());
 }
 
 void LockManager::switchToUser(User *user)
@@ -473,6 +475,12 @@ void LockManager::onBlurWallpaperFinished(const QString &source, const QString &
 
     if (status && m_userWidget->currentUser()->desktopBackgroundPath() == sourcePath)
         emit requestSetBackground(blur);
+}
+
+void LockManager::backgroundChanged(const QString &path)
+{
+    const QString &wallpaper = m_blurImageInter->Get(path);
+    emit requestSetBackground(wallpaper.isEmpty() ? path : wallpaper);
 }
 
 void LockManager::saveUser(User *user)
