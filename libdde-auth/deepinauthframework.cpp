d9eab1942f9b0e3c414218620d0bcb38537c5d9d
justforlxz <justforlxz@gmail.com>
2019-04-19 16:25:57 +0800

    fix(lock): show error at first time

--- libdde-auth/deepinauthframework.cpp
+++ libdde-auth/deepinauthframework.cpp
@@ -3,9 +3,46 @@
 
 #include <QTimer>
 #include <QVariant>
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
 
 static QString USERNAME;
 static QString PASSWORD;
+
+static bool checkUserIsNoPWGrp(const QString &username)
+{
+    // Caution: 32 here is unreliable, and there may be more
+    // than this number of groups that the user joins.
+
+    int ngroups = 32;
+    gid_t groups[32];
+    struct passwd pw;
+    struct group gr;
+
+    /* Fetch passwd structure (contains first group ID for user) */
+
+    pw = *getpwnam(username.toUtf8().data());
+
+    /* Retrieve group list */
+
+    if (getgrouplist(username.toUtf8().data(), pw.pw_gid, groups, &ngroups) == -1) {
+        fprintf(stderr, "getgrouplist() returned -1; ngroups = %d\n",
+                ngroups);
+        return false;
+    }
+
+    /* Display list of retrieved groups, along with group names */
+
+    for (int i = 0; i < ngroups; i++) {
+        gr = *getgrgid(groups[i]);
+        if (QString(gr.gr_name) == QString("nopasswdlogin")) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 DeepinAuthFramework::DeepinAuthFramework(DeepinAuthInterface *inter, QObject *parent)
     : QObject(parent)
@@ -30,8 +67,11 @@ void DeepinAuthFramework::Authenticate()
     m_keyboard->SetUser(USERNAME);
     m_fprint->SetUser(USERNAME);
 
+    if (checkUserIsNoPWGrp(USERNAME) || !PASSWORD.isEmpty()) {
+        QTimer::singleShot(100, m_keyboard, &AuthAgent::Authenticate);
+    }
+
     // It takes time to auth again after cancel!
-    QTimer::singleShot(100, m_keyboard, &AuthAgent::Authenticate);
     QTimer::singleShot(500, m_fprint, &AuthAgent::Authenticate);
 }
 
