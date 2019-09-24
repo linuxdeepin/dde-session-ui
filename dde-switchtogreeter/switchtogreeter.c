8febb4e03cb10616a2dbd86793676abe966d3b50
lixin <lixin_cm@deepin.com>
2019-09-24 17:48:34 +0800

    fix(dde-switchtogreeter): fixed BUG 3216:switch to greeter filed when userName character is too long

--- dde-switchtogreeter/switchtogreeter.c
+++ dde-switchtogreeter/switchtogreeter.c
@@ -172,18 +172,10 @@ static uid_t name_to_uid(char const *name)
     if (!name) {
         return -1;
     }
-    long const buflen = sysconf(_SC_GETPW_R_SIZE_MAX);
-    if (buflen == -1) {
-        return -1;
-    }
-    // requires c99
-    char buf[buflen];
-    struct passwd pwbuf, *pwbufp;
-    if (0 != getpwnam_r(name, &pwbuf, buf, buflen, &pwbufp)
-            || !pwbufp) {
-        return -1;
-    }
-    return pwbufp->pw_uid;
+
+    struct passwd *pwbufp = getpwnam(name);
+
+    return pwbufp ? pwbufp->pw_uid : -1;
 }
 
 // ret:
