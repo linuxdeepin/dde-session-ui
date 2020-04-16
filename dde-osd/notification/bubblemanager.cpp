6f146cc164c9928838277e521401d490adb9d271
zhanghongxiang <zhanghongxiang@uniontech.com>
2020-04-16 14:43:56 +0800

    fix: 修改WiFi连接时，SSID带\，通知显示\\问题
    符号'\'被转义为'\\'
    
    Log: 修改WiFi连接时，SSID带\，通知显示\\问题
    Bug: 19732(bug链接找不到了，相关开发已离职，从miantian/5.0.1合入的bug)
    Change-Id: I6a79f64167f5c4af8a77b90b40f1933eee79099c
    Reviewed-on: http://gerrit.uniontech.com/c/dde-session-ui/+/2047
    Reviewed-by: huangjiawei <huangjiawei@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-osd/notification/bubblemanager.cpp
+++ dde-osd/notification/bubblemanager.cpp
@@ -150,8 +150,11 @@ uint BubbleManager::Notify(const QString &appName, uint replacesId,
              << "actions:" << actions << "hints:" << hints << "expireTimeout:" << expireTimeout;
 #endif
 
+    QString strBody = body;
+    strBody.replace(QLatin1String("\\\\"), QLatin1String("\\"), Qt::CaseInsensitive);
+
     EntityPtr notification = std::make_shared<NotificationEntity>(appName, QString(), appIcon,
-                                                                  summary, body, actions, hints,
+                                                                  summary, strBody, actions, hints,
                                                                   QString::number(QDateTime::currentMSecsSinceEpoch()),
                                                                   QString::number(replacesId),
                                                                   QString::number(expireTimeout),
