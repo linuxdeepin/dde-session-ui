8b8b0a6a652ebb7c68182162632fa0160ba27743
chenwei <chenwei@uniontech.com>
2020-06-10 15:25:33 +0800

    fix: 隐私协议对话框滚动条对话框没有中文翻译
    QT库翻译未加载
    
    Log: 隐私协议对话框滚动条对话框没有中文翻译
    Bug: https://pms.uniontech.com/zentao/bug-view-26239.html
    Change-Id: I881bfdd2497ca58753eb7a75d5b885a3727078e7
    Reviewed-on: http://gerrit.uniontech.com/c/dde-session-ui/+/2051
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: huangjiawei <huangjiawei@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- dde-license-dialog/main.cpp
+++ dde-license-dialog/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 {
     DApplication::loadDXcbPlugin();
     DApplication a(argc, argv);
+    a.loadTranslator();
     //    a.setTheme("light");
 
     QTranslator translator;
