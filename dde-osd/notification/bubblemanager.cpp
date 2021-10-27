d983eeed08b3eec0683f7663a978ce704cc18480
guoyao <guoyao@uniontech.com>
2021-10-27 14:17:59 +0800

    fix: 多次发送通知，并锁屏解锁后，再次发送通知会显示空白气泡
    锁屏后，删除所有气泡，并阻止新的气泡产生
    
    Log: 修复了通知中心会显示空白气泡问题
    Influence: 通知中心
    Bug: https://pms.uniontech.com/zentao/bug-view-97634.html
    Change-Id: I034036a04c4b75a97cc2e59ad0e0acc5f34c35fd

--- dde-osd/notification/bubblemanager.cpp
+++ dde-osd/notification/bubblemanager.cpp
@@ -246,7 +246,9 @@ void BubbleManager::pushBubble(EntityPtr notify)
     if (notify == nullptr) return;
 
     Bubble *bubble = createBubble(notify);
-
+    if (!bubble)
+        return;
+ 
     if (m_bubbleList.size() == BubbleEntities + BubbleOverLap) {
         m_oldEntities.push_front(m_bubbleList.last()->entity());
         m_bubbleList.last()->setVisible(false);
@@ -271,7 +273,8 @@ void BubbleManager::refreshBubble()
     if (m_bubbleList.size() < BubbleEntities + BubbleOverLap + 1 && !m_oldEntities.isEmpty()) {
         auto notify = m_oldEntities.takeFirst();
         Bubble *bubble = createBubble(notify, BubbleEntities + BubbleOverLap - 1);
-        m_bubbleList.push_back(bubble);
+        if (bubble)
+            m_bubbleList.push_back(bubble);
     }
 }
 
@@ -730,6 +733,9 @@ bool BubbleManager::calcReplaceId(EntityPtr notify)
 
 Bubble *BubbleManager::createBubble(EntityPtr notify, int index)
 {
+    //当处于锁屏状态，不允许创建Bubble
+    if (m_userInter->locked())
+        return nullptr;
     Bubble *bubble = new Bubble(nullptr, notify);
     bubble->setMaskAlpha(m_appearance->opacity() * 255);
     connect(m_appearance, &Appearance::OpacityChanged, bubble, &Bubble::onOpacityChanged);
@@ -740,8 +746,9 @@ Bubble *BubbleManager::createBubble(EntityPtr notify, int index)
         if (ptr->isShowInNotifyCenter())
             m_persistence->addOne(ptr);
     });
-    connect(m_userInter, &__SessionManager::LockedChanged, bubble, [=] {
+    connect(m_userInter, &__SessionManager::LockedChanged, bubble, [this, bubble] {
         //当锁屏状态发生变化时隐藏通知，桌面和锁屏的通知不交叉显示
+        popBubble(bubble);
         bubble->hide();
     });
 
